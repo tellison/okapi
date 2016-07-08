ae669a971e094155cd5aed965943d89119e43f60
Tim Ellison <t.p.ellison@gmail.com>
2016-07-08 15:11:47 +0100

    Spice up the utils and add tests for them.

--- src/main/java/com/peir/iot/device/BMP180Utils.java
+++ src/main/java/com/peir/iot/device/BMP180Utils.java
@@ -30,40 +30,50 @@ public class BMP180Utils {
     }
 
     /**
-     * Returns the altitude given the measured and sea level pressures.
+     * Returns the calculated altitude given the sea level and measured
+     * pressures, and temperature at the measured point.
      * 
-     * Given a reference sea level pressure (hPa) and measured pressure (hPa)
-     * calculates the altitude (m) using the international barometric formula.
+     * Given a reference mean sea level pressure (hPa) and measured pressure
+     * (hPa) and temperature (C) calculates the altitude (m) using the
+     * hypsometric formula. Only valid for altitudes within the Troposphere (up
+     * to 11Km above seal level).
      * 
      * @param seaLevelPressure
-     *            pressure at sea level, in hPa
+     *            pressure at mean sea level, in hPa
      * @param measuredPressure
      *            pressure at altitude, in hPa
+     * @param measuredTemperture
+     *            temperature at the measured point, in deg. C.
      * @return altitude above sea level, in metres
      */
-    public static float absoluteAltitude(float seaLevelPressure, float measuredPressure) {
-        double exponent = 1.0d / 5.255d;
-        double temp = 1.0d - Math.pow(measuredPressure / seaLevelPressure, exponent);
-        return (float) (44330.0d * temp);
+    public static float absoluteAltitude(float seaLevelPressure, float measuredPressure, float measuredTemperture) {
+        double term = Math.pow(seaLevelPressure / measuredPressure, (1d / 5.257)) - 1d;
+        double numerator = term * (measuredTemperture + 273.15);
+        return (float) (numerator / 0.0065);
     }
 
     /**
-     * Returns the pressure at sea-level given the known altitude and measured
-     * pressure.
+     * Returns the calculated pressure at mean sea-level given a known altitude
+     * above sea level and measured temperature and pressure at that known
+     * altitude
      * 
-     * Given a reference altitude (m) and measured pressure (hPa) at that
-     * altitude, calculates the pressure at sea level (hPa) using the
-     * international barometric formula.
+     * Given a reference altitude (m) and measured pressure (hPa) an temperature
+     * (C) at that altitude, calculates the pressure at sea level (hPa) using
+     * the international barometric formula for a standard dry atmosphere.
      * 
-     * @param absoluteAltitude
+     * @param heightASL
      *            the altitude above sea level, in meters
      * @param measuredPressure
      *            pressure at altitude, in hPa
+     * @param measuredTemperature
+     *            temperature at altitude, in de. C.
      * @return air pressure at sea level, in hPa
      */
-    public static float sealevelPressure(float absoluteAltitude, float measuredPressure) {
-        double term = 1.0d - (absoluteAltitude / 44330.0d);
-        double denominator = Math.pow(term, 5.255d);
-        return (float) (measuredPressure / denominator);
+    public static float seaLevelPressure(float heightASL, float measuredPressure, float measuredTemperature) {
+        double heightFactor = 0.0065d * heightASL;
+        double denom = heightFactor + measuredTemperature + 273.15;
+        double term = 1.0d - (heightFactor / denom);
+        double powTerm = Math.pow(term, -5.257d);
+        return (float) (measuredPressure * powTerm);
     }
 }
---
+++ src/test/java/com/peir/iot/test/BMP180UtilsTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2016 Tim Ellison
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.peir.iot.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.peir.iot.device.BMP180Utils;
+
+public class BMP180UtilsTest {
+
+    @Test
+    public void testCelsiusToFahrenheit() {
+        // A few well known values
+        assertEquals(32.0f, BMP180Utils.celsiusToFahrenheit(0.0f), 0.001f);
+        assertEquals(212.0f, BMP180Utils.celsiusToFahrenheit(100), 0.001f);
+        assertEquals(-40f, BMP180Utils.celsiusToFahrenheit(-40.0f), 0.001f);
+        assertEquals(98.6f, BMP180Utils.celsiusToFahrenheit(37.0f), 0.001f);
+    }
+
+    @Test
+    public void testAbsoluteAltitude() {
+        // regular values
+        assertEquals(3_255.52, BMP180Utils.absoluteAltitude(1_011f, 700, 19), 0.01f);
+        assertEquals(303.66, BMP180Utils.absoluteAltitude(1067f, 1029, 12), 0.01f);
+        // negative altitude
+        assertEquals(-268.24, BMP180Utils.absoluteAltitude(990f, 1021f, 25), 0.01f);
+        // same pressure
+        assertEquals(0f, BMP180Utils.absoluteAltitude(778, 778, 19), 0.01f);
+    }
+
+    @Test
+    public void testSeaLevelPressure() {
+        // regular values
+        assertEquals(987.64f, BMP180Utils.seaLevelPressure(3_000f, 700f, 15), 0.1f);
+        assertEquals(1_047.51f, BMP180Utils.seaLevelPressure(400f, 1_000f, 20), 0.1f);
+        // same height
+        assertEquals(717.9f, BMP180Utils.seaLevelPressure(0f, 717.9f, 10), 0.01f);
+    }
+}
